cdc69faa2cb388a4e0e1126ddb010a3624f4a66d
Ehsan Noferesti <noferesti.eh@gmail.com>
2022-01-03 20:59:30 +0330

    Updates

---
+++ precache-manifest.ace2f94437af4f882fb1c84b474e636a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb8c59e6eac5667ad6551fe11003401f",
+    "url": "/Maktab_Todo/index.html"
+  },
+  {
+    "revision": "3c3304ce2a078474d1f2",
+    "url": "/Maktab_Todo/static/css/2.5aa997f2.chunk.css"
+  },
+  {
+    "revision": "5f6ad70de66e0451c171",
+    "url": "/Maktab_Todo/static/css/main.3b42b976.chunk.css"
+  },
+  {
+    "revision": "3c3304ce2a078474d1f2",
+    "url": "/Maktab_Todo/static/js/2.5881e23d.chunk.js"
+  },
+  {
+    "revision": "5f6ad70de66e0451c171",
+    "url": "/Maktab_Todo/static/js/main.0ca3beb7.chunk.js"
+  },
+  {
+    "revision": "9d82cd36ad32d78b1212",
+    "url": "/Maktab_Todo/static/js/runtime~main.8bb2dd58.js"
+  }
+]);
